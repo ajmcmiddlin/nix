3ed92b74988497bc1bc844eee667846d74db4895
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-08-28 20:46:33 +1000

    Try bumping back gi-xlib in taffybar overlay.
    2.0.7 isn't in cabal hashes for 19.03. 2.0.2 definitely should be, so let's see
    if it's recent enough.

--- home-manager/home-overlays/taffybar/default.nix
+++ home-manager/home-overlays/taffybar/default.nix
@@ -3,14 +3,14 @@ self: super:
 # Taffybar's default.nix has a nixpkgs pin and overlays that it maintains
 # lets ignore messing with our haskell pkgs and just use it as the toplevel.
 # This is pretty gross and will likely break later, ofc
-let 
+let
   taffybar-json = builtins.fromJSON (builtins.readFile ./git.json);
   taffybar-src  = builtins.fetchGit { inherit (taffybar-json) url rev; };
   taffybar-overlay = _: pkgs: {
     haskellPackages = pkgs.haskellPackages.override (old: {
       overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
         taffybar = self.callCabal2nix "taffybar" taffybar-src { inherit (pkgs) gtk3; };
-        gi-xlib = self.callHackage "gi-xlib" "2.0.7" {};
+        gi-xlib = self.callHackage "gi-xlib" "2.0.2" {};
       });
     });
   };
