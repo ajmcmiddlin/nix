8e28a9f8ca5d75794913bf5e18f844c83581c9ae
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-08-28 14:41:17 +1000

    Add machine specific stuff for hermes.

---
+++ machine.hermes.nix
@@ -0,0 +1,56 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  hostName = "hermes";
+in
+{
+  imports = [ "./shares.${hostName}.nix" ];
+  nix.buildCores = 2;
+
+  networking.hostName = "${hostName}";
+
+  networking.firewall = {
+    enable = true;
+    # allowedTCPPorts = [8080];
+  };
+
+  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
+
+  # LUKS root device
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda2";
+      preLVM = true;
+    }
+  ];
+
+  hardware.pulseaudio.package = pkgs.pulseaudioLight.override { jackaudioSupport = true; };
+
+  # environment.systemPackages = (with pkgs; [
+  #   jack2Full
+  # ]);
+
+  users.groups = { realtime = {}; };
+
+  # Set limits for realtime -- used by JACK
+  security.pam.loginLimits = [
+    { domain = "@realtime";
+      type = "-";
+      item = "rtprio";
+      value = "99";
+    }
+
+    { domain = "@realtime";
+      type = "-";
+      item = "memlock";
+      value = "unlimited";
+    }
+  ];
+
+  users.extraUsers.andrew.extraGroups = ["realtime"];
+}
