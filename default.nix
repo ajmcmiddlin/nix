be6f6095dfb6cc4964b06d4311412c6d3749d986
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-11-15 21:45:33 +1000

    Make VBox extensions configurable.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-machine:
+{ machine, enableVirtualBoxExtensions ? true }:
 { config, pkgs, ... }:
 let
   pwd = ./.;
@@ -111,7 +111,7 @@ in
   # Enable VirtualBox (don't install the package)
   virtualisation.virtualbox.host.enable = true;
   # NOTE: this eats a source build of VirtualBox. Disable if a rebuild is taking too long.
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enableExtensionPack = enableVirtualBoxExtensions;
   virtualisation.docker.enable = true;
 
   # List services that you want to enable:
