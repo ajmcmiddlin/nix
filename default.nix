198e0543b6d4cc291bcb5d16c2f7333d8397086b
Andrew McMiddlin <andrew@ajmccluskey.com>
2020-02-10 08:45:19 +1000

    Comment out hydra.qfpl.io as a cache.
    Apparently it's been stopped.

--- default.nix
+++ default.nix
@@ -22,14 +22,14 @@ in
     ];
 
   nix.binaryCaches = [
-    "https://hydra.qfpl.io"
+    # "https://hydra.qfpl.io"
     "https://cache.nixos.org"
     "https://nixcache.reflex-frp.org"
   ];
 
   nix.binaryCachePublicKeys = [
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-    "qfpl.io:xME0cdnyFcOlMD1nwmn6VrkkGgDNLLpMXoMYl58bz5g="
+    # "qfpl.io:xME0cdnyFcOlMD1nwmn6VrkkGgDNLLpMXoMYl58bz5g="
   ];
 
   # Use the systemd-boot EFI boot loader.
