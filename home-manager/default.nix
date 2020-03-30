1aefda3febb275049090c653666354b408e89f1d
Andrew McMiddlin <andrew@ajmccluskey.com>
2020-03-30 10:30:45 +1000

    Add synergy client.

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -307,6 +307,12 @@ in {
     source = ./dot-files/config/taffybar/taffybar.hs;
     onChange = restart-taffybar;
   };
+
+  services.synergy.client = {
+    enable = true;
+    serverAddress = "192.168.1.101";
+    screenName = "stevie";
+  }
   # home.file.".config/taffybar/taffybar.css" = {
   #   source = ./dot-files/taffybar/taffybar.css;
   #   onChange = restart-taffybar;
