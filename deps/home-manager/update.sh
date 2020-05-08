95e2c0d8c8af5971ee82728e18d73ef64e154b6c
Andrew McMiddlin <andrew@ajmccluskey.com>
2020-05-08 10:50:34 +1000

    Do a crime in bash.
    Fix output of home-manager update to include ref. Without the ref,
    fetchGit won't find the commit we want.

--- deps/home-manager/update.sh
+++ deps/home-manager/update.sh
@@ -8,4 +8,11 @@ if (( $# < 1 )); then
   exit 1
 fi
 
-nix-prefetch-git https://github.com/rycee/home-manager --rev refs/heads/${1} > $HERE/thunk.json
+nix-prefetch-git https://github.com/rycee/home-manager --rev refs/heads/${1} > $HERE/thunk-temp.json
+
+# Cry deeply.
+head -n 2 $HERE/thunk-temp.json > $HERE/thunk.json
+echo "  \"ref\": \"refs/heads/${1}\"," >> $HERE/thunk.json
+tail -n +3 $HERE/thunk-temp.json >> $HERE/thunk.json
+rm $HERE/thunk-temp.json
+
