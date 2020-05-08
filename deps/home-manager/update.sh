ab5fcf5f21c7ab55b9703e6588dacda0b1359af9
Andrew McMiddlin <andrew@ajmccluskey.com>
2020-05-08 10:29:32 +1000

    Fix update script for home-manager and use 20.03.
    Change checkout to the 20.03 release of home manager.

--- deps/home-manager/update.sh
+++ deps/home-manager/update.sh
@@ -3,4 +3,9 @@
 
 # Example usage: ./update.sh --rev release-19.03
 HERE=$(dirname $0)
-nix-prefetch-git https://github.com/rycee/home-manager $1 > $HERE/thunk.json
+if (( $# < 1 )); then
+  echo "Need to provide release branch as first and only argument --- e.g. release-20.03"
+  exit 1
+fi
+
+nix-prefetch-git https://github.com/rycee/home-manager --rev refs/heads/${1} > $HERE/thunk.json
